122f08967dfc9e7f9783df765be5c72dbf3793dd
Copley, Caleb Scott <ccopley7@gatech.edu>
2025-05-22 15:12:42 -0400

    Added two Skills and changed AbstractCard constructor
    Put a new variable in AbtractCard that holds the cards description. Also added two new Skills that do nothing. Those being a Defend and a TakeAim.

--- Assets/Scenes/CardScripts/AbstractBullet.cs
+++ Assets/Scenes/CardScripts/AbstractBullet.cs
@@ -5,7 +5,7 @@ public abstract class AbstractBullet : AbstractCard
     public readonly int DAMAGE;
     public readonly int SPEED;
 
-    public AbstractBullet(string name, int cost, string filePath, int damage, int speed): base(name, cost, filePath){
+    public AbstractBullet(string name, int cost, string filePath, string desc, int damage, int speed): base(name, cost, filePath, desc){
         DAMAGE = damage;
         SPEED = speed;
     }
--- Assets/Scenes/CardScripts/Bullets/SixShooterBullet.cs
+++ Assets/Scenes/CardScripts/Bullets/SixShooterBullet.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class SixShooterBullet : AbstractBullet
 {
-    public SixShooterBullet(): base("Six Shooter Bullet", 2, null, 1, 3){
+    public SixShooterBullet(): base("Six Shooter Bullet", 2, null, "The classic weapon for the classic cowboy", 1, 3){
 
     }
 }
