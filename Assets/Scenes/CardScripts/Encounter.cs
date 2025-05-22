b377edb31621b1bd9d0474b3bf34fbaf53b2b11a
Copley, Caleb Scott <ccopley7@gatech.edu>
2025-05-22 15:03:31 -0400

    Added Hand to Encounter class
    Added an object to store the current Player hand during combat. Currently draws three random cards from the given deck in the Constructor.

--- Assets/Scenes/CardScripts/Encounter.cs
+++ Assets/Scenes/CardScripts/Encounter.cs
@@ -1,14 +1,30 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System;
+using UnityEngine.LightTransport;
 
 public class Encounter
 {
     public List<AbstractCard> currDeck;
     public List<AbstractCard> enemyDeck;
 
+    public List<AbstractCard> hand;
+
+    private int handsize = 3;
+
     public Encounter(List<AbstractCard> currDeck, List<AbstractCard> enemyDeck){
         this.currDeck = new List<AbstractCard>(currDeck);
         this.enemyDeck = new List<AbstractCard>(enemyDeck);
+
+        System.Random rand = new System.Random();
+
+        hand = new List<AbstractCard>();
+        for(int i = 0; i < handsize; i++)
+        {
+            int num = rand.Next(0, this.currDeck.Count);
+            hand.Add(this.currDeck[num]);
+            this.currDeck.RemoveAt(num);
+        }
     }
 
     public override string ToString(){
@@ -23,6 +39,20 @@ public class Encounter
             }
         }
 
+        string handString = "[";
+        for (int i = 0; i < hand.Count; i++)
+        {
+            handString += hand[i];
+            if (i != hand.Count - 1)
+            {
+                handString += ", ";
+            }
+            else
+            {
+                handString += "]";
+            }
+        }
+
         string deck2 = "[";
         for(int i = 0; i < enemyDeck.Count; i++){
             deck2 += enemyDeck[i];
@@ -34,7 +64,7 @@ public class Encounter
             }
         }
 
-        return "Player Deck: " + deck1 + "\nEnemy Deck: " + deck2;
+        return "Player Deck: " + deck1 + "\nPlayer Hand: " + handString + "\nEnemy Deck"+deck2;
     }
 
 }
