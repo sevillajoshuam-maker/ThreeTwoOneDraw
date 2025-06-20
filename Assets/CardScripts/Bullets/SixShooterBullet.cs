d9ddadb106931640989e46b7fad369076695c8dd
Copley, Caleb Scott <ccopley7@gatech.edu>
2025-06-20 14:27:57 -0400

    Integrated new Sprites
    Resized and added new sprites into the static Image Library.

--- Assets/CardScripts/Bullets/SixShooterBullet.cs
+++ Assets/CardScripts/Bullets/SixShooterBullet.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class SixShooterBullet : AbstractBullet
 {
     //0 argument constructor that makes a basic six shooter bullet
-    public SixShooterBullet(): base("Six Shooter Bullet", 3, ImageLibrary.dark_take_aim_concept_art, 
+    public SixShooterBullet(): base("Six Shooter Bullet", 3, ImageLibrary.sixShooter_art, 
                                     "Fire one SLOW bullet that deals 25 damage", 25, Speed.Slow, ImageLibrary.default_bullet_concept_art, ImageLibrary.default_superBullet_concept_art){
 
     }
