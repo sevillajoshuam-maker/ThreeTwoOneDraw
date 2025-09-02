ed3bf939ec98c7c502cea3867a5d9977083cdfef
ccopley04 <ccopley7@gatech.edu>
2025-09-01 22:06:39 -0400

    Update CardBlueprint

--- Assets/CardScripts/CardBlueprint.cs
+++ Assets/CardScripts/CardBlueprint.cs
@@ -15,7 +15,7 @@ public class CardBlueprint : AbstractSkill
     public CardBlueprint():base(nameOfCard, costOfCard, imageOfCard, descOfCard){}
 
     //Override the use() method and put whatever the card does when it activates here
-    public override void use(AbstractPlayer user){
+    public override void use(AbstractPlayer user, float duration){
         Debug.Log("The Default Card Was Played");
     }
 }
