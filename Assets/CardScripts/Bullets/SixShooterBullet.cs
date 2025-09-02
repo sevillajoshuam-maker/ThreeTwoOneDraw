436ad4ff7e3da61132094418868036960f6aed94
omagarwal25 <lidagarwal@gmail.com>
2025-09-01 20:38:06 -0400

    refactor for better damage calculation

--- Assets/CardScripts/Bullets/SixShooterBullet.cs
+++ Assets/CardScripts/Bullets/SixShooterBullet.cs
@@ -3,15 +3,17 @@ using UnityEngine;
 public class SixShooterBullet : AbstractBullet
 {
     //0 argument constructor that makes a basic six shooter bullet
-    public SixShooterBullet(): base("Six Shooter Bullet", 3, ImageLibrary.sixShooter_art, 
-                                    "Fire one SLOW bullet that deals 25 damage", 25, Speed.Slow, ImageLibrary.default_bullet_concept_art, ImageLibrary.default_superBullet_concept_art){
-
+    public SixShooterBullet() : base("Six Shooter Bullet", 3, ImageLibrary.sixShooter_art,
+        "Fire one SLOW bullet that deals 25 damage", 25, Speed.Slow, ImageLibrary.default_bullet_concept_art,
+        ImageLibrary.default_superBullet_concept_art)
+    {
     }
 
     //When the card is played, call the fire method with this bullet passed
     //Also propogate the shooter to the fire() method to dictate where the bullet spawns/bullet direction
-    public override void use(AbstractPlayer user){
+    public override void use(AbstractPlayer user)
+    {
         Debug.Log(this);
-       BulletManager.Instance.fire(user, this);
+        BulletManager.Instance.fire(user, this);
     }
 }
