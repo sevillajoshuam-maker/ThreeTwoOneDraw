e3702880f3ed451db2df50d441519c1dce694517
omagarwal25 <lidagarwal@gmail.com>
2025-09-04 15:12:30 -0400

    Added winchester!

---
+++ Assets/CardScripts/Bullets/WinchesterBullet.cs
@@ -0,0 +1,20 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class WinchesterBullet : AbstractBullet
+{
+    // TODO: change art
+    public WinchesterBullet() : base("Winchester Bullet", 2, ImageLibrary.sixShooter_art,
+        "Fire one FAST bullet that deals 5 damage", 5, Speed.Fast, ImageLibrary.default_bullet_concept_art,
+        ImageLibrary.default_superBullet_concept_art, SoundType.SixShooterBullet)
+    {
+    }
+
+    //When the card is played, call the fire method with this bullet passed
+    //Also propogate the shooter to the fire() method to dictate where the bullet spawns/bullet direction
+    public override void use(AbstractPlayer user, float duration)
+    {
+        BulletManager.Instance.fire(user, this, this.sound);
+    }
+}
