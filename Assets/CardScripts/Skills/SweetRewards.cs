5934008cb7751988f612667bf554d17dcb67559a
ccopley04 <ccopley7@gatech.edu>
2025-09-06 15:29:53 -0400

    Tested And Fixed SweetRewards
    Added a small fix to the SweetRewards script and changed card to default art.

--- Assets/CardScripts/Skills/SweetRewards.cs
+++ Assets/CardScripts/Skills/SweetRewards.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class SweetRewards : AbstractSkill
 {
-    public SweetRewards() : base("Sweet Rewards", 4, ImageLibrary.takeAim_art, "Draw three cards instantly.") { }
+    public SweetRewards() : base("Sweet Rewards", 4, ImageLibrary.default_card, "Draw three cards instantly.") { }
 
     public override void use(AbstractPlayer user, float duration){
         for (int i = 0; i < 3; i++)
         {
             EncounterControl.Instance.currPlayer.Draw();
+
         }
+        EncounterControl.Instance.reapplyHand();
     }
 }
