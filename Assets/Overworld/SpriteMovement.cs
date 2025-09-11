d6039aa16d8550dd9f0dfce9981145b5185303f3
ccopley04 <ccopley7@gatech.edu>
2025-09-10 22:43:17 -0400

    Added sprite
    Added the sprite images to the overworld and created a collider trigger to start combat.

--- Assets/Overworld/SpriteMovement.cs
+++ Assets/Overworld/SpriteMovement.cs
@@ -2,21 +2,32 @@ using UnityEngine;
 
 public class SpriteMovement : MonoBehaviour
 {
+    [SerializeField]
+    private Sprite[] sprites;
+    private SpriteRenderer rendr;
 
+    void Start(){
+        rendr = gameObject.GetComponent<SpriteRenderer>();
+        rendr.sprite = sprites[0];
+    }
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKey(KeyCode.DownArrow)){
             gameObject.transform.position -= new Vector3(0, 0.01f,0);
+            rendr.sprite = sprites[0];
         }
         else if(Input.GetKey(KeyCode.UpArrow)){
             gameObject.transform.position += new Vector3(0, 0.01f,0);
+            rendr.sprite = sprites[2];
         }
         else if(Input.GetKey(KeyCode.RightArrow)){
             gameObject.transform.position += new Vector3(0.01f, 0,0);
+            rendr.sprite = sprites[3];
         }
         else if(Input.GetKey(KeyCode.LeftArrow)){
             gameObject.transform.position -= new Vector3(0.01f, 0,0);
+            rendr.sprite = sprites[1];
         }
 
     }
