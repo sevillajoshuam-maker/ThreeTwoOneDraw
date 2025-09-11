89cb0847aeabb5463c7972c3912f344a94b2cd83
ccopley04 <ccopley7@gatech.edu>
2025-09-11 19:05:29 -0400

    Added walking sound
    Added a sound manager to the overworld scene. Also added footsteps sounds that play when ever the player moves in any direction.

--- Assets/Overworld/SpriteMovement.cs
+++ Assets/Overworld/SpriteMovement.cs
@@ -6,6 +6,9 @@ public class SpriteMovement : MonoBehaviour
     private Sprite[] sprites;
     private SpriteRenderer rendr;
 
+    private float count = 0;
+
+
     void Start(){
         rendr = gameObject.GetComponent<SpriteRenderer>();
         rendr.sprite = sprites[0];
@@ -16,19 +19,32 @@ public class SpriteMovement : MonoBehaviour
         if(Input.GetKey(KeyCode.DownArrow)){
             gameObject.transform.position -= new Vector3(0, 0.01f,0);
             rendr.sprite = sprites[0];
+            walkingSound();
         }
         else if(Input.GetKey(KeyCode.UpArrow)){
             gameObject.transform.position += new Vector3(0, 0.01f,0);
             rendr.sprite = sprites[2];
+            walkingSound();
         }
         else if(Input.GetKey(KeyCode.RightArrow)){
             gameObject.transform.position += new Vector3(0.01f, 0,0);
             rendr.sprite = sprites[3];
+            walkingSound();
         }
         else if(Input.GetKey(KeyCode.LeftArrow)){
             gameObject.transform.position -= new Vector3(0.01f, 0,0);
             rendr.sprite = sprites[1];
+            walkingSound();
         }
+    }
 
+    private void walkingSound(){
+        if(!SoundManager.audioSource.isPlaying){
+            SoundManager.playSound(SoundType.WildWestWalking);
+            count = 10f;
+        }
+        else{
+            count -= 0.01f;
+        }
     }
 }
