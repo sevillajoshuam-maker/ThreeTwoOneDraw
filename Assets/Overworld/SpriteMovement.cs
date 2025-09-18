58508bbfa30dad181b9d995dd19994b2df75c687
Eugene Li <eugene.gao.li@gmail.com>
2025-09-18 09:43:35 -0400

    Movement Script Overhaul
    Cleaned up movement code and redid it so its more editable and more readable

--- Assets/Overworld/SpriteMovement.cs
+++ Assets/Overworld/SpriteMovement.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using System;
 public class SpriteMovement : MonoBehaviour
 {
     [SerializeField]
@@ -7,34 +7,43 @@ public class SpriteMovement : MonoBehaviour
     private SpriteRenderer rendr;
 
     private float count = 0;
-
+    public float speed = 10;
 
     void Start(){
         rendr = gameObject.GetComponent<SpriteRenderer>();
         rendr.sprite = sprites[0];
     }
     // Update is called once per frame
+    void Move(double x, double y, int animFrame)
+    {
+        //complicated math equations so that moving diagonally isn't faster than walking straight
+        double x_y_Squared = Math.Pow(x, 2) + Math.Pow(y, 2);
+        double a = x / Math.Sqrt(x_y_Squared);
+        double b = y / Math.Sqrt(x_y_Squared);
+        float i = (float)a; //math equations done in double datatype but vectors are done in floats
+        float j = (float)b; //i and j convert double a and b into float datatype
+        gameObject.transform.position += new Vector3(i, j, 0) * speed * Time.deltaTime;
+        rendr.sprite = sprites[animFrame];
+        walkingSound();
+    }   
     void Update()
     {
-        if(Input.GetKey(KeyCode.DownArrow)){
-            gameObject.transform.position -= new Vector3(0, 0.01f,0);
-            rendr.sprite = sprites[0];
-            walkingSound();
+        double horizontal = Input.GetAxisRaw("Horizontal");
+        double vertical = Input.GetAxisRaw("Vertical");
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            Move(horizontal, vertical, 0);
         }
-        else if(Input.GetKey(KeyCode.UpArrow)){
-            gameObject.transform.position += new Vector3(0, 0.01f,0);
-            rendr.sprite = sprites[2];
-            walkingSound();
+        else if (Input.GetKey(KeyCode.UpArrow)) {
+            Move(horizontal, vertical, 2);
+
         }
-        else if(Input.GetKey(KeyCode.RightArrow)){
-            gameObject.transform.position += new Vector3(0.01f, 0,0);
-            rendr.sprite = sprites[3];
-            walkingSound();
+        else if (Input.GetKey(KeyCode.RightArrow)) {
+            Move(horizontal, vertical, 3);
+
         }
-        else if(Input.GetKey(KeyCode.LeftArrow)){
-            gameObject.transform.position -= new Vector3(0.01f, 0,0);
-            rendr.sprite = sprites[1];
-            walkingSound();
+        else if (Input.GetKey(KeyCode.LeftArrow)) {
+            Move(horizontal, vertical, 1);
+
         }
     }
 
