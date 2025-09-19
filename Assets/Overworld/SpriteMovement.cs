732a5a5e340e7bae241da19431447c852d085fbd
Eugene Li <eugene.gao.li@gmail.com>
2025-09-19 02:16:47 -0400

    More Movement overhaul
    made movement more code efficient and readable. Also implemented the animation for bill walking

--- Assets/Overworld/SpriteMovement.cs
+++ Assets/Overworld/SpriteMovement.cs
@@ -2,58 +2,65 @@ using UnityEngine;
 using System;
 public class SpriteMovement : MonoBehaviour
 {
+
     [SerializeField]
-    private Sprite[] sprites;
-    private SpriteRenderer rendr;
-
+    public Animator anim;
     private float count = 0;
-    public float speed = 10;
+    public float moveSpeed = 10;
+    private Vector3 input;
+    private bool isMoving = false;
+    private float i = 0;
+    private float j = 0;
 
-    void Start(){
-        rendr = gameObject.GetComponent<SpriteRenderer>();
-        rendr.sprite = sprites[0];
+    void Update()
+    {
+        double horizontal = Input.GetAxisRaw("Horizontal");
+        double vertical = Input.GetAxisRaw("Vertical");
+        Move(horizontal, vertical);
+        Animate();
     }
-    // Update is called once per frame
-    void Move(double x, double y, int animFrame)
+    void Move(double x, double y)
     {
         //complicated math equations so that moving diagonally isn't faster than walking straight
         double x_y_Squared = Math.Pow(x, 2) + Math.Pow(y, 2);
         double a = x / Math.Sqrt(x_y_Squared);
         double b = y / Math.Sqrt(x_y_Squared);
-        float i = (float)a; //math equations done in double datatype but vectors are done in floats
-        float j = (float)b; //i and j convert double a and b into float datatype
-        gameObject.transform.position += new Vector3(i, j, 0) * speed * Time.deltaTime;
-        rendr.sprite = sprites[animFrame];
-        walkingSound();
-    }   
-    void Update()
-    {
-        double horizontal = Input.GetAxisRaw("Horizontal");
-        double vertical = Input.GetAxisRaw("Vertical");
-        if (Input.GetKey(KeyCode.DownArrow)) {
-            Move(horizontal, vertical, 0);
-        }
-        else if (Input.GetKey(KeyCode.UpArrow)) {
-            Move(horizontal, vertical, 2);
-
-        }
-        else if (Input.GetKey(KeyCode.RightArrow)) {
-            Move(horizontal, vertical, 3);
-
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow)) {
-            Move(horizontal, vertical, 1);
-
-        }
+        i = (float)x; //math equations done in double datatype but vectors are done in floats
+        j = (float)y; //i and j convert double a and b into float datatype
+        input = new Vector3(i, j, 0);
+        gameObject.transform.position +=  input* moveSpeed * Time.deltaTime;
     }
+    
 
-    private void walkingSound(){
-        if(!SoundManager.audioSource.isPlaying){
+    private void walkingSound()
+    {
+        if (!SoundManager.audioSource.isPlaying)
+        {
             SoundManager.playSound(SoundType.WildWestWalking);
             count = 10f;
         }
-        else{
+        else
+        {
             count -= 0.01f;
         }
+    }
+    private void Animate()
+    {
+        if (input.magnitude > 0.1f || input.magnitude < -0.1f)
+        {
+            isMoving = true;
+            walkingSound();
+        }
+        else
+        {
+            isMoving = false;
+        }
+        if (isMoving)
+        {
+            anim.SetFloat("x", i);
+            anim.SetFloat("y", j);
+
+        }
+        anim.SetBool("isMoving",isMoving);
     }
 }
