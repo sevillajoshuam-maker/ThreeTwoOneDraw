6841c31d3b4ce8dc0091939c9f84619fd5f3d35a
ccopley04 <ccopley7@gatech.edu>
2025-09-22 22:12:01 -0400

    Error checking for enemy behaviour
    For enemy behaviour, added an error check for the index. Also created a basic Enemy state machine which will be the template.

--- Assets/PlayerScripts/Bandit.cs
+++ Assets/PlayerScripts/Bandit.cs
@@ -12,11 +12,9 @@ public class Bandit : Enemy{
     public Bandit(): base(new List<AbstractCard>(), 100, 1, "Bandit"){
         for (int i = 0; i < 12; i++)
         {
-            if (random.Next(3) == 0)
-                deck.Add(new TakeAim());
-            if (random.Next(2) == 0)
-                deck.Add(new Defend());
             if (random.Next(1) == 0)
+                deck.Add(new Defend());
+            if (random.Next(2) == 0)
                 deck.Add(new SixShooterBullet());
         }
     }
